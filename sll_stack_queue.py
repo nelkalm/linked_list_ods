cb13aa733a198fcfac74ea9537a11729bef46c5a
Nelson Lu <nelkalmlu@gmail.com>
2022-05-12 09:22:01 -0700

    Commit with __str__ method for printing

--- sll_stack_queue.py
+++ sll_stack_queue.py
@@ -7,9 +7,31 @@ class LinkedList:
         self.head = None
         self.tail = None
     
+    def __str__(self):
+
+        # defining a blank result variable
+        result = ''
+
+        # initializing pointer to head
+        node = self.head
+
+        # traversing SLL and adding to head
+        while node:
+            result += str(node.value) + ', '
+            node = node.next
+        
+        # removing trailing commas
+        result = result.strip(', ')
+
+        # checking if anything is present in result
+        if len(result):
+            return '[' + result + ']'
+        else:
+            return '[]'
+    
     def push(self, value):
         new_node = Node(value)
-        new_node.set_next = self.head
+        new_node.next = self.head
         self.head = new_node
         if self.length == 0:
             self.tail = new_node
@@ -34,8 +56,29 @@ class LinkedList:
         if self.length == 0:
             self.head = added_node
         else:
-            self.tail = added_node
+            self.tail.next = added_node
         self.tail = added_node
         self.length += 1
         return True
 
+
+if __name__ == '__main__':
+    my_sll = LinkedList()
+    print(my_sll)
+
+    my_sll.add('a')
+    my_sll.add('b')
+    my_sll.add('c')
+    my_sll.add('d')
+    my_sll.add('e')
+    my_sll.add('x')
+    print(my_sll)
+
+    my_sll.remove()
+    print(my_sll)
+
+    my_sll.pop()
+    print(my_sll)
+
+    my_sll.push('y')
+    print(my_sll)
