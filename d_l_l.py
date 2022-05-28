f4c05dcea0fabe5c73de06435ecc016ca65321b9
Nelson Lu <nelkalmlu@gmail.com>
2022-05-27 17:48:29 -0700

    more methods to commit

--- d_l_l.py
+++ d_l_l.py
@@ -56,7 +56,7 @@ class DoublyLinkedList:
             temp.prev = None
         self.length -= 1
         return temp
-    
+
     def pop_first(self):
         if self.length == 0:
             return False
@@ -71,6 +71,44 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            prev_node = self.get(index - 1)
+            next_node = prev_node.next
+            prev_node.next = new_node
+            new_node.prev = prev_node
+            new_node.next = next_node
+            next_node.prev = new_node
+            self.length += 1
+            return True
+
 
 my_doubly_linked_list = DoublyLinkedList(None)
 my_doubly_linked_list.print_list()
@@ -80,4 +118,8 @@ my_doubly_linked_list.prepend(3)
 my_doubly_linked_list.prepend(4)
 my_doubly_linked_list.print_list()
 my_doubly_linked_list.pop_first()
+my_doubly_linked_list.print_list()
+my_doubly_linked_list.set_value(1, 4)
+my_doubly_linked_list.print_list()
+my_doubly_linked_list.insert(1, 'a')
 my_doubly_linked_list.print_list()
