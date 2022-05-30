08e12822c77dfa05a18f5dd4a385d04e6851010d
Nelson Lu <nelkalmlu@gmail.com>
2022-05-30 13:09:38 -0700

    stack commit

--- stack.py
+++ stack.py
@@ -32,10 +32,17 @@ class Stack:
         top_node.next = None
         self.height -= 1
         return top_node
+    
+    def peek(self):
+        if self.top is None:
+            return None
+        return self.top.value
 
 
 my_stack = Stack(4)
 my_stack.push(1)
+my_stack.push(2)
 my_stack.print_stack()
-my_stack.pop()
-my_stack.print_stack()
+print(my_stack.peek())
+# my_stack.pop()
+# my_stack.print_stack()
